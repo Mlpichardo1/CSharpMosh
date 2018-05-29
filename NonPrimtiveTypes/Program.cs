197ae62b423779643b8c6945bf705c094003e0ba
Jenny Pichardo <mlpichardo1@gmail.com>
2018-05-29 18:22:24 -0400

    lesson 4 complete

--- NonPrimtiveTypes/Program.cs
+++ NonPrimtiveTypes/Program.cs
@@ -6,7 +6,16 @@ namespace NonPrimtiveTypes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var a = 10;
+            var b = a;
+            b++;
+            System.Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+            
+            var array1 = new int[3] {1, 2, 3};
+            var array2 = array1;
+            array1[0] = 0;
+            System.Console.WriteLine();
         }
+
     }
 }
